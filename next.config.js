94180ad2f7c03f0c7239c4f36d10169bdb3a73f2
indiigo-o <edin.gondzo@edu.fit.ba>
2022-07-14 12:25:02 +0200

    aa

--- next.config.js
+++ next.config.js
@@ -1,30 +1,7 @@
-const withPlugins = require('next-compose-plugins')
-const sass = require('@zeit/next-sass')
-const css = require('@zeit/next-css')
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: function(config){
-  config.module.rules.push({
-      test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
-      use: {
-      loader: 'url-loader',
-          options: {
-          limit: 100000,
-          name: '[name].[ext]'
-      }}
-  })
-  return config
-  },
-  env: {
-    BaseUrl:any= 'https://localhost:44308/'
-  }
+  reactStrictMode: true,
+  swcMinify: true,
 }
 
-module.exports = withPlugins([
-    [css],
-    [sass, {
-        cssModules: true
-    }]
-], nextConfig)
-
-
+module.exports = nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,3 +5,5 @@ import { AppProps } from 'next/app';
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
+
+export default MyApp;
